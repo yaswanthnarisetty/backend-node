bb3601fb26b5f582a785937cfe2611b73ca22256
Venkata Yuga Yaswanth Narisetty <venkatayugayaswanth.narisetty@resmed.com.au>
2023-07-07 06:43:00 +0530

    mongo url

--- models/config.js
+++ models/config.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 
 mongoose.set('strictQuery', false);
-export const mon = mongoose.connect('mongodb+srv://yash:Yash1234@crusdb.zjtgnoc.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true});
+export const mon = mongoose.connect('mongodb://127.0.0.1:27017/cruddb',{useNewUrlParser:true});
 
 
 
